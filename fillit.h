1ea48c68ce293cea4609bebd8935911c2ecee2a3
Gaetan DURON <gduron@e2r6p1.42.fr>
2017-04-17 10:25:26 +0200

    added 2 struc to the header

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: gduron <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/17 09:49:13 by gduron            #+#    #+#             */
-/*   Updated: 2017/04/17 09:49:57 by gduron           ###   ########.fr       */
+/*   Updated: 2017/04/17 10:24:19 by gduron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,22 @@
 # define FILLIT_H
 
 
+typedef struct	s_tmino
+{
+	char		**str;
+	int			x;
+	int			y;
+	int			best_x;
+	int			best_y;
+
+}				t_tmino;
+
+
+typedef struct	s_env
+{
+	char		*input_str;
+	t_tmino		**tab_tmino;
+	int			grid_size;
+	int			min_size;
+}				t_env;
 #endif
