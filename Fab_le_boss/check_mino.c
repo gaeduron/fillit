c279c1219e4104640ebe0eaa5ecaffbc6173da62
Naina fabien RAJAONARISON <rajaonarisonfabien@yahoo.fr>
2017-04-18 16:34:36 +0200

    fun checking **minoz and *minoz

--- Fab_le_boss/check_mino.c
+++ Fab_le_boss/check_mino.c
@@ -6,7 +6,7 @@
 /*   By: narajaon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/17 18:02:05 by narajaon          #+#    #+#             */
-/*   Updated: 2017/04/18 09:53:12 by narajaon         ###   ########.fr       */
+/*   Updated: 2017/04/18 16:33:11 by narajaon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,34 @@ typedef struct s_valid
 	int		pos4;
 }				t_valid;
 
-int		check_mino(t_valid mino)
+int		check_tmino(char *mino, t_valid **valid_mino)
+{
+	return (1);
+}
+
+int		check_str(char *mino, t_valid *valid_mino, t_valid **tab_mino)
+{
+	int i;
+	int hei;
+
+	i = 0;
+	hei = 0;
+	if (ft_strlen(mino) != 20)
+		return (0);
+	while (mino[i])
+	{
+		if (mino[i] == '\n')
+			hei++;
+		i++;
+	}
+	if (hei != valid_mino->heigh)
+		return (0);
+	if (!(check_tmino(mino, tab_mino)))
+		return (0);
+	return (1);
+}
+
+int		check_all(char **tab)
 {
 	return (1);
 }
@@ -42,7 +69,7 @@ int		check_tab(t_valid mino, char **tab)
 			return (0);
 		i++;
 	}
-	if (!(check_mino(mino)))
+	if (!(check_all(tab)))
 		return (0);
 	return (1);
 }
