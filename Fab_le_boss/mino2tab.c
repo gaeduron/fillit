8fd46c1f64e0cf3a71f7eedf0e9bf66151983d5d
Naina fabien RAJAONARISON <rajaonarisonfabien@yahoo.fr>
2017-04-18 14:09:31 +0200

    added mino2tab

--- Fab_le_boss/mino2tab.c
+++ Fab_le_boss/mino2tab.c
@@ -6,7 +6,7 @@
 /*   By: narajaon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/17 18:01:15 by narajaon          #+#    #+#             */
-/*   Updated: 2017/04/18 09:08:34 by narajaon         ###   ########.fr       */
+/*   Updated: 2017/04/18 14:09:00 by narajaon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 #include "../fillit.h"
 #define BUFF_SIZE 5000
 
-static int		nb_mino(char *str)
+int		nb_mino(char *str)
 {
 	int nb;
 	int mino;
 
-	nb = 0;
+	nb = 1;
 	mino = 0;
 	while (*str)
 	{
@@ -36,7 +36,43 @@ static int		nb_mino(char *str)
 	return (mino);
 }
 
-int		main()
+void	write_mino(t_tmino *mino, char **grid)
+{
+	char **ptr;
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	ptr = grid;
+	ptr = ptr + mino->y;
+	*ptr = *ptr + mino->x;
+	while (mino->str[j] && ptr[j])
+	{
+		i = 0;
+		while (mino->str[j][i] && ptr[j][i])
+		{
+			if (mino->str[j][i] == '#')
+				ptr[j][i] = mino->str[j][i];
+			i++;
+		}
+		j++;
+	}
+}
+
+t_tmino	**init_tmino(char **tab, int nb_mino)
+{
+	t_tmino **mino;
+	int i;
+
+	i = 0;
+	if (!(mino = (t_tmino **)malloc(sizeof(t_tmino *) * (nb_mino + 1))))
+		return (NULL);
+
+	return (mino);
+}
+
+int		main(int ac, char **av)
 {
 	char **tab;
 	char *buff;
@@ -45,25 +81,16 @@ int		main()
 	int i;
 
 	i = 0;
-	if ((fd = open("valid_sample", O_RDONLY)) < 0)
-	{
-		ft_putstr_fd("error\n", 2);
-		return (0);
-	}
-	if (!(buff = ft_strnew(BUFF_SIZE)))
-	{
-		ft_putstr_fd("error\n", 2);
-		return (0);
-	}
+	fd = open(av[1], O_RDONLY);
+	buff = ft_strnew(BUFF_SIZE);
 	read(fd, buff, BUFF_SIZE);
 	mino = nb_mino(buff);
-//	tab = ft_strsplit(buff, '$');
-
-	/*while (tab[i])
+	tab = ft_strsplit(buff, '$');
+	while (tab[i])
 	{
 		printf("%s\n", tab[i]);
 		i++;
-	}*/
+	}
 	printf("%s\n", buff);
 	printf("nb mino: %d\n", mino);
 	return (0);
