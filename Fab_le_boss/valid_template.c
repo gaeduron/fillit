0a8d65daee1349f3907b675694650eb7c74cc815
Naina fabien RAJAONARISON <rajaonarisonfabien@yahoo.fr>
2017-04-19 13:33:06 +0200

    template checks done

--- Fab_le_boss/valid_template.c
+++ Fab_le_boss/valid_template.c
@@ -6,7 +6,7 @@
 /*   By: narajaon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/18 17:54:32 by narajaon          #+#    #+#             */
-/*   Updated: 2017/04/18 20:04:29 by narajaon         ###   ########.fr       */
+/*   Updated: 2017/04/19 13:31:12 by narajaon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@
 #define MINO3 mino->pos3
 #define MINO4 mino->pos4
 
-int		template1(t_valid *mino)
+/* figure en T*/
+
+int		template_T(t_valid *mino)
 {
 	if ((MINO2[0] == MINO1[0] && MINO2[1] == MINO1[1] + 1)\
 			&& (MINO3[0] == MINO1[0] && MINO3[1] == MINO1[1] + 2)\
@@ -38,24 +40,94 @@ int		template1(t_valid *mino)
 	return (0);
 }
 
-//a faire
-int		template2(t_valid *mino)
+/*figure en L-1*/
+
+int		template_L1(t_valid *mino)
 {
+	if ((MINO2[0] == MINO1[0] + 1 && MINO2[1] == MINO1[1])\
+			&& (MINO3[0] == MINO1[0] + 2 && MINO3[1] == MINO1[1] - 1)\
+			&& (MINO4[0] == MINO1[0] + 2 && MINO4[1] == MINO1[1]))
+		return (1);
+	if ((MINO2[0] == MINO1[0] + 1 && MINO2[1] == MINO1[1])\
+			&& (MINO3[0] == MINO1[0] + 1 && MINO3[1] == MINO1[1] + 1)\
+			&& (MINO4[0] == MINO1[0] + 1 && MINO4[1] == MINO1[1] + 2))
+		return (1);
+	if ((MINO2[0] == MINO1[0] && MINO2[1] == MINO1[1] + 1)\
+			&& (MINO3[0] == MINO1[0] && MINO3[1] == MINO1[1])\
+			&& (MINO4[0] == MINO1[0] + 2 && MINO4[1] == MINO1[1]))
+		return (1);
+	if ((MINO2[0] == MINO1[0] + 1 && MINO2[1] == MINO1[1])\
+			&& (MINO3[0] == MINO1[0] && MINO3[1] == MINO1[1] + 1)\
+			&& (MINO4[0] == MINO1[0] && MINO4[1] == MINO1[1] + 2))
+		return (1);
+	return (0);
+}
+
+int		template_L2(t_valid *mino)
+{
+	if ((MINO2[0] == MINO1[0] + 1 && MINO2[1] == MINO1[1])\
+			&& (MINO3[0] == MINO1[0] + 2 && MINO3[1] == MINO1[1])\
+			&& (MINO4[0] == MINO1[0] + 2 && MINO4[1] == MINO1[1] + 1))
+		return (1);
+	if ((MINO2[0] == MINO1[0] && MINO2[1] == MINO1[1] + 1)\
+			&& (MINO3[0] == MINO1[0] && MINO3[1] == MINO1[1] + 2)\
+			&& (MINO4[0] == MINO1[0] + 1 && MINO4[1] == MINO1[1]))
+		return (1);
+	if ((MINO2[0] == MINO1[0] && MINO2[1] == MINO1[1] + 1)\
+			&& (MINO3[0] == MINO1[0] + 1 && MINO3[1] == MINO1[1] + 1)\
+			&& (MINO4[0] == MINO1[0] + 2 && MINO4[1] == MINO1[1] + 1))
+		return (1);
+	if ((MINO2[0] == MINO1[0] + 1 && MINO2[1] == MINO1[1] - 2)\
+			&& (MINO3[0] == MINO1[0] + 1 && MINO3[1] == MINO1[1] - 1)\
+			&& (MINO4[0] == MINO1[0] + 1 && MINO4[1] == MINO1[1]))
+		return (1);
+	return (0);
+}
+
+int		template_I(t_valid *mino)
+{
+	if ((MINO2[0] == MINO1[0] + 1 && MINO2[1] == MINO1[1])\
+			&& (MINO3[0] == MINO1[0] + 2 && MINO3[1] == MINO1[1])\
+			&& (MINO4[0] == MINO1[0] + 3 && MINO4[1] == MINO1[1]))
+		return (1);
 	if ((MINO2[0] == MINO1[0] && MINO2[1] == MINO1[1] + 1)\
 			&& (MINO3[0] == MINO1[0] && MINO3[1] == MINO1[1] + 2)\
 			&& (MINO4[0] == MINO1[0] + 1 && MINO4[1] == MINO1[1] + 3))
 		return (1);
-	if ((MINO2[0] == MINO1[0] + 1 && MINO2[1] == MINO1[1] - 1)\
-			&& (MINO3[0] == MINO1[0] + 2 && MINO3[1] == MINO1[1])\
-			&& (MINO4[0] == MINO1[0] + 3 && MINO4[1] == MINO1[1]))
+	return (0);
+}
+
+int		template_Z1(t_valid *mino)
+{
+	if ((MINO2[0] == MINO1[0] && MINO2[1] == MINO1[1] + 1)\
+			&& (MINO3[0] == MINO1[0] + 2 && MINO3[1] == MINO1[1] + 1)\
+			&& (MINO4[0] == MINO1[0] + 2 && MINO4[1] == MINO1[1] + 2))
 		return (1);
 	if ((MINO2[0] == MINO1[0] + 1 && MINO2[1] == MINO1[1] - 1)\
-			&& (MINO3[0] == MINO1[0] + 2 && MINO3[1] == MINO1[1])\
-			&& (MINO4[0] == MINO1[0] + 3 && MINO4[1] == MINO1[1]))
+			&& (MINO3[0] == MINO1[0] + 1 && MINO3[1] == MINO1[1])\
+			&& (MINO4[0] == MINO1[0] + 2 && MINO4[1] == MINO1[1] - 1))
+		return (1);
+	return (0);
+}
+
+int		template_Z2(t_valid *mino)
+{
+	if ((MINO2[0] == MINO1[0] && MINO2[1] == MINO1[1] + 1)\
+			&& (MINO3[0] == MINO1[0] + 1 && MINO3[1] == MINO1[1] - 1)\
+			&& (MINO4[0] == MINO1[0] + 1 && MINO4[1] == MINO1[1]))
 		return (1);
 	if ((MINO2[0] == MINO1[0] + 1 && MINO2[1] == MINO1[1])\
-			&& (MINO3[0] == MINO1[0] + 2 && MINO3[1] == MINO1[1] + 1)\
-			&& (MINO4[0] == MINO1[0] + 3 && MINO4[1] == MINO1[1]))
+			&& (MINO3[0] == MINO1[0] + 1 && MINO3[1] == MINO1[1] + 1)\
+			&& (MINO4[0] == MINO1[0] + 2 && MINO4[1] == MINO1[1] + 1))
+		return (1);
+	return (0);
+}
+
+int		template_sqrt(t_valid *mino)
+{
+	if ((MINO2[0] == MINO1[0] && MINO2[1] == MINO1[1] + 1)\
+			&& (MINO3[0] == MINO1[0] + 1 && MINO3[1] == MINO1[1])\
+			&& (MINO4[0] == MINO1[0] + 1 && MINO4[1] == MINO1[1] + 1))
 		return (1);
 	return (0);
 }
@@ -72,6 +144,6 @@ int		main(void)
 	MINO3[1] = 1;
 	MINO4[0] = 3;
 	MINO4[1] = 1;
-	printf("%d", template1(mino));
+	printf("%d", template_L2(mino));
 	return (0);
 }
